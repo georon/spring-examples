98953297a1ee07d4c7512d4ad04f025719547799
Ron Geo <icancertify@gmail.com>
2021-01-16 23:13:22 -0500

    corrections 16-jan-2021

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,4 @@
 
-drop table "address" if exists;
-
-CREATE TABLE "address" (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
-  name VARCHAR(250) NOT NULL,
-  address VARCHAR(250) DEFAULT NULL
-);
-
 INSERT INTO "address" (name, address) VALUES
   ('Bill Gates', 'Billionaire Road - Washington'),
   ('Kumar Valla', 'Valanjavattom');
